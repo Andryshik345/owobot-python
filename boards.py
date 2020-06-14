8192c21bb87bfcb10d38732f2d7a0230119e6882
Andryshik345 <andryshik9@gmail.com>
2020-06-14 22:39:41 +0500

    Rewrite on Python

---
+++ boards.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+"""
+    Names of subreddits with weeb pictures.
+"""
+    
+SFW = [
+    "awwnime",
+    "wholesomeyuri",
+    "Joshi_Kosei",
+    "AnimeBlush",
+    "tsunderes",
+    "Usagimimi",
+    "kitsunemimi",
+    "animeponytails",
+    "tyingherhairup",
+    "OfficialSenpaiHeat",
+    "ChurchofBelly",
+    "Moescape",
+    "MoeStash",
+    "TwoDeeArt",
+    "2DArtchive",
+    "headpats",
+    "cutelittlefangs",
+    "kemonomimi",
+    "twintails",
+    "pouts",
+    "gao",
+    "animelegs",
+    "ZettaiRyouiki",
+    "thighdeology",
+    "Animewallpaper",
+    "pantsu",
+    "Patchuu",
+    "animehotbeverages",
+    "silverhair",
+    "longhairedwaifus",
+    "shorthairedwaifus",
+    "Smugs"
+    ]
+    
+NSFW = [
+    "ecchi",
+    "hentai",
+    "Tentai",
+    "MonsterGirl",
+    #"HENTAI_GIF",
+    "animemidriff",
+    "AnimeBooty",
+    "hentaifemdom",
+    "dekaihentai",
+    "rule34",
+    "thick_hentai",
+    "hentaibondage",
+    "HumanCumflation",
+    "BigAnimeTiddies",
+    "HentaiPetgirls",
+    "LoweredPantyhose",
+    "skindentation",
+    "doujinshi",
+    "CumHentai",
+    "waifusgonewild",
+    "UpskirtHentai",
+    "CedehsHentai",
+    "Artistic_Hentai",
+    "HentaiSchoolGirls",
+    "HentaiAnal",
+    "yuri",
+    "muchihentai",
+    "AnimeMILFS",
+    "Nekomimi",
+    "KuroiHada"
+    ]
